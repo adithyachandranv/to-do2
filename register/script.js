a3a7630195c93cd79436689ea585f19727f651b5
adithyachandranv <adithyachandranv@gmail.com>
2023-10-30 00:52:07 +0530

    register page1

---
+++ register/script.js
@@ -0,0 +1,15 @@
+function heading_animation() {
+    gsap.from('#heading', {
+        y: 10,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.3,
+        stagger: 0.3
+    })
+}
+heading_animation();
+
+
+
+
+
